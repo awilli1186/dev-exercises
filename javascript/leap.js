33132c6d0333987c5e01fad94e6b9b6e22425783
Adam Williams <awilli1186@gmail.com>
2015-11-16 17:23:46 -0600

    forgot to save leap.js

---
+++ javascript/leap.js
@@ -0,0 +1,11 @@
+var isLeapYear = function(year) {
+  if((year % 4 == 0) && (year % 100 != 0)) {
+    return true;
+  } else if ((year % 4 == 0) && (year % 400 == 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+module.exports = isLeapYear;
